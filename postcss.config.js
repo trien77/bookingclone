1c3c840453aca9b7835224a83714a64d2297715b
Trienle <ledieptrien@gmail.com>
2023-10-11 06:32:46 +0800

    first-commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+
+
+export default{
+    plugins: {
+      'postcss-import': {}, // Helps to @import CSS files
+      'autoprefixer': {},   // Adds vendor prefixes to CSS rules
+      'cssnano': {},        // Minifies CSS
+      'tailwindcss': {},
+      'autoprefixer': {},
+      // You can add more plugins as needed
+    }
+  }
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div  className="text-red-500">
+      Hello there
+    </div>
+  )
+}
+
+export default App
